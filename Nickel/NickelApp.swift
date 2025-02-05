11c80f60e3f7ba23378ffe29cd2875adc594144d
TfourJ <48729601+tfourj@users.noreply.github.com>
2025-02-05 19:47:36 +0100

    Initial Commit

---
+++ Nickel/NickelApp.swift
@@ -0,0 +1,17 @@
+//
+//  NickelApp.swift
+//  Nickel
+//
+//  Created by TfourJ on 5. 2. 25.
+//
+
+import SwiftUI
+
+@main
+struct NickelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NickelTests/NickelTests.swift
@@ -0,0 +1,17 @@
+//
+//  NickelTests.swift
+//  NickelTests
+//
+//  Created by TfourJ on 5. 2. 25.
+//
+
+import Testing
+@testable import Nickel
+
+struct NickelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
